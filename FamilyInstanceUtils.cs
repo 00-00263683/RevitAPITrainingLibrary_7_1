91bb18d52d35509bf73e6acd479739fedc81c8bf
00-00263683 <matrosda@gmail.com>
2022-02-08 11:19:28 +0300

    Добавьте файлы проекта.

---
+++ FamilyInstanceUtils.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+
+namespace RevitAPITrainingLibrary_7_1
+{
+    public class FamilyInstanceUtils
+    {
+        public static FamilyInstance CreateFamilyInstance(
+            ExternalCommandData commandData,
+            FamilySymbol oFamSymb,
+            XYZ insertionPoint,
+            Level oLevel)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Autodesk.Revit.DB.Document doc = uidoc.Document;
+            FamilyInstance familyInstance = null;
+            //create family instance
+            using (var t = new Autodesk.Revit.DB.Transaction(doc, "Create family instance"))
+            {
+                t.Start();
+                if (!oFamSymb.IsActive)
+                {
+                    oFamSymb.Activate();
+                    doc.Regenerate();
+                }
+                familyInstance = doc.Create.NewFamilyInstance(
+                                    insertionPoint,
+                                    oFamSymb,
+                                    oLevel,
+                                    Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                t.Commit();
+            }
+            return familyInstance;
+        }
+
+        public static IEnumerable<FamilySymbol> GetSheetTemplate(ExternalCommandData commandData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ViewPortUtils.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary_7_1
+{
+    public class ViewPortUtils
+    {
+        public static List<ViewPlan> GetViewPorts(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var views = new FilteredElementCollector(doc)
+                .OfClass(typeof(ViewPlan))
+                .Cast<ViewPlan>()
+                .ToList();
+            return views;
+        }
+    }
+}
